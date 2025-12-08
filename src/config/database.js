41462c2da291935e99ab1dcc9cbcea4a49d41878
GP Dhanush <agprakash406@gmail.com>
2025-12-08 17:25:39 +0530

    updated db configs

--- src/config/database.js
+++ src/config/database.js
@@ -2,14 +2,14 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const db = mysql.createPool({
-  // host: process.env.DB_HOST,
-  // user: process.env.DB_USER,
-  // password: process.env.DB_PASSWORD,
-  // database: process.env.DB_NAME
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'prasowla_moi_master'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+  // host: 'localhost',
+  // user: 'root',
+  // password: '',
+  // database: 'prasowla_moi_master'
 
 });
 
